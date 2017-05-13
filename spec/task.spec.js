aaaace7d6521a8532cfa0ec2bf13bd65abb67883
Matthew Alexander LaChance <mattlach@umich.edu>
2017-05-13 17:00:04 -0400

    :white_check_mark: Adding a tomato increments the counter

--- spec/task.spec.js
+++ spec/task.spec.js
@@ -254,6 +254,11 @@ describe('a default task object', function() {
     it('has one tomato', function() {
       expect(task.get_tomatoes_length()).toBe(1);
     });
+
+    it('has two tomatoes after loading a second', function() {
+      task.add_tomato(Tomato('second tomato'));
+      expect(task.get_tomatoes_length()).toBe(2);
+    });
   });
 });
 
